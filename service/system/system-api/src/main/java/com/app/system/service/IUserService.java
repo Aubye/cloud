88e7ba589d343b73ba01659a1081e9b902ab8d2d
sunjc <sunjc@live.cn>
2019-03-13 08:18:47 +0800

    update REST interface

--- service/system/system-api/src/main/java/com/app/system/service/IUserService.java
+++ service/system/system-api/src/main/java/com/app/system/service/IUserService.java
@@ -2,10 +2,15 @@ package com.app.system.service;
 
 import com.app.system.domain.dto.UserDTO;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.util.List;
 
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 @Path("/user")
 public interface IUserService {
 
