2395d8576379a65177e48ee581dece66c0cd5fa3
sunjc <sunjc@startimes.com.cn>
2019-03-16 17:49:40 +0800

    add jersey settings

---
+++ common/common-base/src/main/java/com/app/common/config/JerseyConfig.java
@@ -0,0 +1,11 @@
+package com.app.common.config;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JerseyConfig extends ResourceConfig {
+    public JerseyConfig() {
+        packages("com.app");
+    }
+}
