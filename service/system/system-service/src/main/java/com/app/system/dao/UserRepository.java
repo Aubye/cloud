cc85ee0a99a231c250ee66152f0748218fb10a82
sunjc <sunjc@startimes.com.cn>
2019-03-28 13:18:23 +0800

    add jquery and data

--- service/system/system-service/src/main/java/com/app/system/dao/UserRepository.java
+++ service/system/system-service/src/main/java/com/app/system/dao/UserRepository.java
@@ -2,7 +2,8 @@ package com.app.system.dao;
 
 import com.app.system.domain.pojo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
 }
--- service/system/system-service/src/main/java/com/app/system/service/UserServiceImpl.java
+++ service/system/system-service/src/main/java/com/app/system/service/UserServiceImpl.java
@@ -4,9 +4,15 @@ import com.app.common.utils.BeanMapper;
 import com.app.system.dao.UserRepository;
 import com.app.system.domain.dto.UserDTO;
 import com.google.common.collect.Lists;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.criteria.Predicate;
 import java.util.List;
 
 @Service
@@ -20,6 +26,27 @@ public class UserServiceImpl implements IUserService {
 
     @Override
     public String getUser() {
+        Specification specification = (root, query, cb) -> {
+            List<Predicate> predicates = Lists.newArrayList();
+            List<Long> userIds = Lists.newArrayList();
+            userIds.add(1L);
+            if (CollectionUtils.isNotEmpty(userIds)) {
+                Predicate predicate = root.get("id").in(userIds);
+                predicates.add(predicate);
+            }
+            predicates.add(cb.equal(root.get("id"), 1));
+            return cb.and(predicates.toArray(new Predicate[predicates.size()]));
+        };
+
+        Integer pageNum = 1;
+        Integer pageLimit = 1;
+        PageRequest id = new PageRequest(1, 10, new Sort(new Sort.Order(Sort.Direction.DESC, "id")));
+        PageRequest pageRequest = PageRequest.of(pageNum - 1, pageLimit, Sort.by(Sort.Direction.DESC, "id"));
+        Page page = userRepository.findAll(specification, pageRequest);
+        List dataList = beanMapper.mapAsList(page, UserDTO.class);
+        long totalElements = page.getTotalElements();
+        long totalPages = page.getTotalPages();
+
         return "Test User...";
     }
 
