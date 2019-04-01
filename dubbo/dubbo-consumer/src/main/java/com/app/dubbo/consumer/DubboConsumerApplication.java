fb8fcbf5dca445d1de02694927b85f411249059f
sunjc <sunjc@startimes.com.cn>
2019-04-01 17:55:47 +0800

    add dubbo project

---
+++ dubbo/dubbo-consumer/src/main/java/com/app/dubbo/consumer/DubboConsumerApplication.java
@@ -0,0 +1,11 @@
+package com.app.dubbo.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DubboConsumerApplication.class, args);
+    }
+}
---
+++ dubbo/dubbo-producter/src/main/java/com/app/dubbo/producter/DubboProducterApplication.java
@@ -0,0 +1,11 @@
+package com.app.dubbo.producter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboProducterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DubboProducterApplication.class, args);
+    }
+}
