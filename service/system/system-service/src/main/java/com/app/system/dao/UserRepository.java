24d744375e2fcc4ff2ae2ac16cc73ccb1d96fa7a
sunjc <sunjc@live.cn>
2019-05-21 00:02:34 +0800

    update some dependencies

--- service/system/system-service/src/main/java/com/app/system/dao/UserRepository.java
+++ service/system/system-service/src/main/java/com/app/system/dao/UserRepository.java
@@ -4,6 +4,10 @@ import com.app.system.domain.pojo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    List<User> findByName(String name);
 
 }
